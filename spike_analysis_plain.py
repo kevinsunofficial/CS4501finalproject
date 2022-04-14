182ee0c538f925b4912079a71827893a982504e0
kevinsunofficial <kevin.sun0612@hotmail.com>
2022-04-13 22:31:00 -0400

    add plain version of spike analysis

--- spike_analysis_plain.py
+++ spike_analysis_plain.py
@@ -8,13 +8,6 @@ from Bio.pairwise2 import format_alignment
 from Bio.SeqRecord import SeqRecord
 from tqdm import tqdm
 
-import multiprocessing
-
-def aligning(seq_info):
-    seq_id, seq2 = seq_info
-    alignment = pairwise2.align.globalms(refseq,seq2,2,-1,-10,-0.5,one_alignment_only=True,penalize_end_gaps=False)
-    return (seq_id, alignment[0].seqB)
-    # return seq_id, seq2
 
 if __name__=='__main__':
     fastafn = 'sars_cov2_s_genomic.fasta'
@@ -28,7 +21,6 @@ if __name__=='__main__':
     df.head()
 
     refseq = sequences['NC_045512.2']
-    align_seq = [(seq_id,sequences[seq_id]) for seq_id in tqdm(df.Accession.tolist(), desc='prepare alignemnt')]
 
     s_sequences = [
         SeqRecord(
@@ -39,11 +31,11 @@ if __name__=='__main__':
         )
     ]
 
-    workers = multiprocessing.Pool(5)
-    results = workers.map_async(aligning, align_seq).get()
-
-    for seq_info in tqdm(results, desc='parsing result'):
-        seq_id, seqB = seq_info
+    for seq_id in tqdm(df.Accession.tolist(), desc='aligning'):
+        seq2 = sequences[seq_id]
+        alignment = pairwise2.align.globalms(refseq,seq2,2,-1,-10,-0.5,one_alignment_only=True,penalize_end_gaps=False)
+        seqB = alignment[0].seqB
+        
         s_sequences.append(
             SeqRecord(
                 seqB,
